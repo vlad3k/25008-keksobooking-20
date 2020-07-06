35b31412462ca293e9f6bdd3414688a070627eff
vlad3k <vlad3k@bk.ru>
2020-07-06 21:51:04 +0300

    add constants for sizes photo

--- js/constants.js
+++ js/constants.js
@@ -3,9 +3,13 @@
 window.constants = (function () {
   var PIN_WIDTH = 50;
   var PIN_HEIGHT = 70;
+  var PHOTO_WIDTH = 45;
+  var PHOTO_HEIGHT = 40;
 
   return {
     PIN_WIDTH: PIN_WIDTH,
     PIN_HEIGHT: PIN_HEIGHT,
+    PHOTO_WIDTH: PHOTO_WIDTH,
+    PHOTO_HEIGHT: PHOTO_HEIGHT,
   };
 })();
